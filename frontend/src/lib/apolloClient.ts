da556572f760ea6874aa5dd622a7b18e44343800
aetherblue77 <aetherblue77@gmail.com>
2026-02-06 19:48:43 +0700

    subgraph problem

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
 
-const SUBGRAPH_URL = process.env.NEXT_PUBLIC_SUBGRAPH_URL || ""
+const SUBGRAPH_URL = "https://api.studio.thegraph.com/query/1740311/aether-staking-vault/version/latest"
 
 const client = new ApolloClient({
     link: new HttpLink({
